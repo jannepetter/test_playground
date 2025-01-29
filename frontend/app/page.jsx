457fe2db663d3e9dfab387690bff230341a5ad2e
jannepetter <peterdev80@gmail.com>
2025-01-29 18:02:48 +0200

    test update 3

--- frontend/app/page.jsx
+++ frontend/app/page.jsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main>
       <div className="mx-auto max-w-md">
-        <h1 className="my-10">The appi</h1>
+        <h1 className="my-10">The app</h1>
       </div>
     </main>
   );
